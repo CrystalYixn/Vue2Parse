17d8e5f2bdab338d4812471beb417385c4553d2f
CrystalYixn <1095175021@qq.com>
2022-09-28 22:40:26 +0800

    fix: 修改追踪

--- src/state.js
+++ src/state.js
@@ -7,8 +7,6 @@ export function initState(vm) {
 
 function initData(vm) {
   let data = vm.$options.data
-  console.log(2, vm)
   data = typeof data === 'function' ? data.call(vm) : data
   debugger
-  console.log(3, data)
 }
