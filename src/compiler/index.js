c2a26b40f2e3e3e5e5575f5817803dd3eb5a1d8b
CrystalYixn <1095175021@qq.com>
2022-10-02 12:44:32 +0800

    feat: 模板解析

---
+++ src/compiler/index.js
@@ -0,0 +1,3 @@
+export function compileToFunciton(template) {
+  console.log(template)
+}
--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 import { initState } from "./state"
+import { compileToFunciton } from "./compiler/index"
 
 export function initMixin(Vue) {
   Vue.prototype._init = function(options) {
@@ -6,5 +7,24 @@ export function initMixin(Vue) {
     vm.$options = options // 初始化时将用户选项挂载到实例上，方便其他扩展方法读取
 
     initState(vm)
+
+    if (options.el) {
+      vm.$mount(options.el)
+    }
+  }
+
+  Vue.prototype.$mount = function(el) {
+    const vm = this
+    el = document.querySelector(el)
+    const ops = vm.$options
+    if (!ops.render) {
+      let template
+      if (ops.template) {
+        template = ops.template
+      } else if (el) {
+        template = el.outerHTML
+      }
+      ops.render = compileToFunciton(template)
+    }
   }
 }
