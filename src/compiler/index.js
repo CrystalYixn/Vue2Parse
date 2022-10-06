148eae47fdfb79905eb2687bb7973022ec695854
CrystalYixn <1095175021@qq.com>
2022-10-06 11:37:42 +0800

    feat: 代码生成原理

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,120 +1,65 @@
-// 匹配合法的标签名称，允许以_开头，中部允许使用- my-div
-const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`
-// 捕获一个合法标签名组成的分组，允许使用命名空间:符号 my-div:test
-const qnameCapture = `((?:${ncname}\\:)?${ncname})`
-// 匹配开始标签 <my-div:test
-const startTagOpen = new RegExp(`^<${qnameCapture}`)
-// 匹配一个结束标签
-const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`)
-// 匹配属性，支持不带值或者单引号或者不带引号外的内容
-const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
-// 匹配开始标签对应的结束标签，支持自闭合标签
-const startTagClose = /^\s*(\/?)>/
+import { parseHTML } from "./parse";
 // 匹配 mustach 语法
 const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g
 
-function parseHTML(html) {
-  const ELEMENT_TYPE = 1
-  const TEXT_TYPE = 3
-  const stack = []
-  let currentParent
-  let root
-  while(html) {
-    // 为 0 代表开始或结束标签，大于 0 代表文本结束位置
-    let textEnd = html.indexOf('<')
-    if (textEnd === 0) {
-      const startTagMatch = parseStartTag()
-      if (startTagMatch) {
-        start(startTagMatch.tagName, startTagMatch.attrs)
-        continue
-      }
-      const endTagMatch = html.match(endTag)
-      if (endTagMatch) {
-        end(endTagMatch[1])
-        advance(endTagMatch[0].length)
-      }
-    } else if (textEnd > 0) {
-      const text = html.substring(0, textEnd)
-      chars(text)
-      advance(text.length)
+export function compileToFunciton(template) {
+  // 1. 解析 Ast
+  let ast = parseHTML(template)
+  // 2. 生成 render 函数
+  console.log(codegen(ast))
+}
+
+function codegen(ast) {
+  let children = genChildren(ast.children)
+  let code = `_c('${ast.tag}', ${ ast.attrs.length ? genProps(ast.attrs) : 'null' }${ ast.children.length ? `, ${children}` : '' })`
+  return code
+}
+
+function genProps(attrs) {
+  let str = ''
+  attrs.forEach(attr => {
+    if (attr.name === 'style') {
+      attr.value = attr.value.split(';').filter(i => i).reduce((preVal, curVal) => {
+        const [key, val] = curVal.split(':')
+        preVal[key] = val
+        return preVal
+      }, {})
     }
-  }
-  console.log(root)
+    str += `${attr.name}:${JSON.stringify(attr.value)},`
+  })
+  return `{${str.slice(0, -1)}}`
+}
 
-  function stackPop() {
-    stack.pop()
-    currentParent = stack[stack.length - 1]
-  }
-
-  function stackPush(node) {
-    stack.push(node)
-    currentParent && currentParent.children.push(node)
-    currentParent = node
-  }
-
-  function createAstElement(tag, attrs, text) {
-    return {
-      tag,
-      type: text ? TEXT_TYPE : ELEMENT_TYPE,
-      attrs,
-      children: [],
-      text,
-      parent: currentParent || null,
-    }
-  }
-
-  /*  */
-  function start(tag, attrs, text) {
-    const node = createAstElement(...arguments)
-    if (!root) {
-      root = node
-    }
-    stackPush(node)
-  }
-
-  /*  */
-  function chars(text) {
-    text = text.replace(/\s/g, '')
-    if (text) {
-      start(undefined, undefined, text)
-      end()
-    }
-  }
-
-  /*  */
-  function end() {
-    stackPop()
-  }
-
-  /* 处理开始标签 */
-  function parseStartTag() {
-    const start = html.match(startTagOpen)
-    if (start) {
-      const match = {
-        tagName: start[1],
-        attrs: [],
-      }
-      advance(start[0].length)
-      let attr, end
-      // 匹配标签属性直到开始标签结束，如果没匹配到开始标签对应的结束标签 && 继续匹配属性
-      while (!(end = html.match(startTagClose)) && (attr = html.match(attribute))) {
-        advance(attr[0].length)
-        match.attrs.push({ name: attr[1], value: attr[3] || attr[4] || attr[5] })
-      }
-      if (end) {
-        advance(end[0].length)
-      }
-      return match
-    }
-    return false
-  }
-
-  // 步进字符串
-  function advance(n) {
-    html = html.substring(n)
+function genChildren(children) {
+  if (children) {
+    return children.map(child => gen(child)).join(',')
   }
 }
 
-export function compileToFunciton(template) {
-  let ast = parseHTML(template)
+function gen(node) {
+  if (node.type === 1) {
+    return codegen(node)
+  } else {
+    const { text } = node
+    if (defaultTagRE.test(text)) {
+      let tokens = []
+      let match
+      let lastIndex = 0
+      defaultTagRE.lastIndex = 0
+      while (match = defaultTagRE.exec(text)) {
+        let index = match.index
+        if (index > lastIndex) {
+          tokens.push(JSON.stringify(text.slice(lastIndex, index)))
+        }
+        tokens.push(`_s(${match[1].trim()})`)
+        lastIndex = index + match[0].length
+      }
+      if (lastIndex < text.length) {
+        tokens.push(JSON.stringify(text.slice(lastIndex)))
+      }
+      return `_v(${ tokens.join('+') })`
+    } else {
+      return `_v(${JSON.stringify(text)})`
+    }
+  }
 }
---
+++ src/compiler/parse.js
@@ -0,0 +1,114 @@
+// 匹配合法的标签名称，允许以_开头，中部允许使用- my-div
+const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`
+// 捕获一个合法标签名组成的分组，允许使用命名空间:符号 my-div:test
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`
+// 匹配开始标签 <my-div:test
+const startTagOpen = new RegExp(`^<${qnameCapture}`)
+// 匹配一个结束标签
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`)
+// 匹配属性，支持不带值或者单引号或者不带引号外的内容
+const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
+// 匹配开始标签对应的结束标签，支持自闭合标签
+const startTagClose = /^\s*(\/?)>/
+
+export function parseHTML(html) {
+  const ELEMENT_TYPE = 1
+  const TEXT_TYPE = 3
+  const stack = []
+  let currentParent
+  let root
+  while(html) {
+    // 为 0 代表开始或结束标签，大于 0 代表文本结束位置
+    let textEnd = html.indexOf('<')
+    if (textEnd === 0) {
+      const startTagMatch = parseStartTag()
+      if (startTagMatch) {
+        start(startTagMatch.tagName, startTagMatch.attrs)
+        continue
+      }
+      const endTagMatch = html.match(endTag)
+      if (endTagMatch) {
+        end(endTagMatch[1])
+        advance(endTagMatch[0].length)
+      }
+    } else if (textEnd > 0) {
+      const text = html.substring(0, textEnd)
+      chars(text)
+      advance(text.length)
+    }
+  }
+  return root
+
+  function stackPop() {
+    stack.pop()
+    currentParent = stack[stack.length - 1]
+  }
+
+  function stackPush(node) {
+    stack.push(node)
+    currentParent && currentParent.children.push(node)
+    currentParent = node
+  }
+
+  function createAstElement(tag, attrs, text) {
+    return {
+      tag,
+      type: text ? TEXT_TYPE : ELEMENT_TYPE,
+      attrs,
+      children: [],
+      text,
+      parent: currentParent || null,
+    }
+  }
+
+  /*  */
+  function start(tag, attrs, text) {
+    const node = createAstElement(...arguments)
+    if (!root) {
+      root = node
+    }
+    stackPush(node)
+  }
+
+  /*  */
+  function chars(text) {
+    text = text.replace(/\s/g, '')
+    if (text) {
+      start(undefined, undefined, text)
+      end()
+    }
+  }
+
+  /*  */
+  function end() {
+    stackPop()
+  }
+
+  /* 处理开始标签 */
+  function parseStartTag() {
+    const start = html.match(startTagOpen)
+    if (start) {
+      const match = {
+        tagName: start[1],
+        attrs: [],
+      }
+      advance(start[0].length)
+      let attr, end
+      // 匹配标签属性直到开始标签结束，如果没匹配到开始标签对应的结束标签 && 继续匹配属性
+      while (!(end = html.match(startTagClose)) && (attr = html.match(attribute))) {
+        advance(attr[0].length)
+        match.attrs.push({ name: attr[1], value: attr[3] || attr[4] || attr[5] })
+      }
+      if (end) {
+        advance(end[0].length)
+      }
+      return match
+    }
+    return false
+  }
+
+  // 步进字符串
+  function advance(n) {
+    html = html.substring(n)
+  }
+}
